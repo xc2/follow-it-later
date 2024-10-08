e8d6c1cc7a61645ddff5ceebe69be608f833726d
Kaffi Y. <gh@109cafe.com>
2024-10-08 19:15:14 +0800

    feat: eager discover readable content (#9)

--- src/content/read.ts
+++ src/content/read.ts
@@ -1,14 +1,15 @@
 import { InboxEntryInput } from "@/types";
-import { Readability, isProbablyReaderable } from "@mozilla/readability";
+import { Readability } from "@mozilla/readability";
 import DOMPurify from "dompurify";
+
 export interface ReaderInfo extends NonNullable<ReturnType<Readability["parse"]>> {
   pured: string;
 }
 
 export function getReaderInfo(): ReaderInfo | false {
-  const isReaderable = isProbablyReaderable(document);
-  if (!isReaderable) return false;
-  const p = new Readability(document.cloneNode(true) as Document).parse()!;
+  const p = new Readability(document.cloneNode(true) as Document).parse();
+  if (!p) return false;
+
   const pured = DOMPurify.sanitize(p?.content || "");
   return { ...p, pured };
 }
