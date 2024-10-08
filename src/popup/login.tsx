8f5c219358a054f6b9b7c99a4a755187a72b2f93
xc2 <gh@109cafe.com>
2024-10-08 16:56:27 +0800

    feat: guiding creating inbox; guiding feedback

--- src/popup/login.tsx
+++ src/popup/login.tsx
@@ -1,12 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { Button, ButtonProps } from "@/components/ui/button";
+import { AvatarIcon } from "@radix-ui/react-icons";
 
-export function Login() {
-  const login = () => {
-    window.open("https://app.follow.is/login", "_blank");
-  };
+export function Login({ purpose, ...props }: { purpose: string } & ButtonProps) {
   return (
-    <Button type="button" onClick={() => login()}>
-      Login Follow
-    </Button>
+    <div>
+      <Button asChild size="sm" type="button" {...props}>
+        <a href="https://app.follow.is/login" target="_blank" rel="noreferrer">
+          <AvatarIcon className="mr-2 h-4 w-4" />
+          Log in Follow
+        </a>
+      </Button>
+      {!purpose || <div className="text-zinc-500 mt-2">{purpose}</div>}
+    </div>
   );
 }
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { handleFollowResponse, isFollowError } from "@/lib/follow";
 import { Login } from "@/popup/login";
 import { SendForm } from "@/popup/send-form";
 import { InboxItem } from "@/types";
+import { Pencil2Icon, PlusIcon } from "@radix-ui/react-icons";
+import { useMemo } from "react";
 import useSWR from "swr";
 
 export function Popup() {
@@ -20,34 +24,80 @@ export function Popup() {
     return chrome.storage.local.get(key).then((r) => r[key]);
   });
   const needLogin = isFollowError(inboxesSWR.error) && inboxesSWR.error.name === "AuthError";
+  const lastUsedInbox = useMemo(() => {
+    return inboxesSWR.data?.find((v) => v.id === lastUsedInboxSWR.data)?.id;
+  }, [inboxesSWR.data, lastUsedInboxSWR.data]);
 
   return (
-    <Card className="border-none rounded-none min-w-80">
-      <CardHeader>
-        <CardTitle>Send to Follow</CardTitle>
-        <CardDescription>
-          A readable version of this page will be sent to your Follow's inbox.
-        </CardDescription>
-      </CardHeader>
-      {!inboxesSWR.isLoading && !lastUsedInboxSWR.isLoading ? (
-        needLogin ? (
-          <CardContent>
-            <Login />
-          </CardContent>
-        ) : (
-          <SendForm inboxes={inboxesSWR.data!} defaultValues={{ inbox: lastUsedInboxSWR.data }} />
-        )
-      ) : (
-        <CardContent>
-          <div className="flex flex-col space-y-5">
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[50px] rounded-xl" />
-              <Skeleton className="h-10 w-[200px]" />
-            </div>
-            <Skeleton className="h-10 w-[150px]" />
+    <TooltipProvider>
+      <div className="min-w-96">
+        <Card className="border-none rounded-none shadow-none">
+          <CardHeader>
+            <CardTitle>Send to Follow</CardTitle>
+            <CardDescription>
+              A readable version of this page will be sent to your Follow's inbox.
+            </CardDescription>
+          </CardHeader>
+          {!inboxesSWR.isLoading && !lastUsedInboxSWR.isLoading ? (
+            needLogin ? (
+              <CardContent>
+                <Login size="sm" purpose="Log in Follow is required to retrieve the inbox list." />
+              </CardContent>
+            ) : inboxesSWR?.data?.length ? (
+              <SendForm inboxes={inboxesSWR.data!} defaultValues={{ inbox: lastUsedInbox }} />
+            ) : (
+              <CardContent>
+                <Button asChild type="button" size="sm">
+                  <a
+                    href="https://app.follow.is/discover?type=inbox"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <PlusIcon className="mr-2 h-4 w-4" />
+                    Create a inbox to continue
+                  </a>
+                </Button>
+              </CardContent>
+            )
+          ) : (
+            <CardContent>
+              <div className="flex flex-col space-y-5">
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-[50px] rounded-xl" />
+                  <Skeleton className="h-10 w-[200px]" />
+                </div>
+                <Skeleton className="h-10 w-[150px]" />
+              </div>
+            </CardContent>
+          )}
+        </Card>
+        <div className="flex border-t-[1px] border-zinc-50 py-2">
+          <div className="min-w-0 flex-initial text-zinc-500 pl-2">
+            This extension is <strong>UNOFFICIAL</strong>. Please DO NOT open issues on Follow's
+            official repository or social accounts.
           </div>
-        </CardContent>
-      )}
-    </Card>
+          <Tooltip>
+            <TooltipTrigger asChild className="ml-auto mr-1">
+              <Button
+                variant="ghost"
+                size="icon"
+                aria-label="Feedback"
+                asChild
+                aria-describedby="feedback-content"
+              >
+                <a href="https://tldr.ws/follow-it-later-feedback" target="_blank" rel="noreferrer">
+                  <Pencil2Icon className="h-4 w-4" />
+                </a>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent className="max-w-[90vw] min-w-0">
+              <p id="feedback-content">
+                If you got any trouble using this extension, please send your feedback here.
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      </div>
+    </TooltipProvider>
   );
 }
