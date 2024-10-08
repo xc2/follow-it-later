cafd254625ad85d5349e867d0abf0b922cb7a0c0
Kaffi Y. <gh@109cafe.com>
2024-10-08 18:01:47 +0800

    ci: deploy (#7)

---
+++ .github/ci-deploy.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! "$GITHUB_REF_TYPE" = "tag" ]]; then
+  echo "Not a tag. Skipping deployment." >&2
+  exit 1
+fi
+
+EXTENSION_VERSION=${GITHUB_REF_NAME#v}
+
+if [[ -z "$EXTENSION_VERSION" ]]; then
+  echo "Could not determine extension version from tag name." >&2
+  exit 2
+fi
+
+echo "Building extension version $EXTENSION_VERSION..."
+
+rm -rf dist *.zip
+env "EXTENSION_VERSION=$EXTENSION_VERSION" pnpm vite build
+cd dist
+zip -r "../follow-it-later.v${EXTENSION_VERSION}.zip" .
